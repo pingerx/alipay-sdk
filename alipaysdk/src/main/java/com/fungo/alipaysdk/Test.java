088ec93a0103ed50022cfbb22a0b234e85293306
Pinger <pinger@xiaomingsport.com>
2019-01-30 14:49:57 +0800

    Initial commit

---
+++ alipaysdk/src/main/java/com/fungo/alipaysdk/Test.java
@@ -0,0 +1,13 @@
+package com.fungo.alipaysdk;
+
+/**
+ * @author Pinger
+ * @since 2019/1/30 14:43
+ */
+public class Test {
+
+    public void main(String[] args) {
+        System.out.println("-------> 哈哈");
+
+    }
+}
