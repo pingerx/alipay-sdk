e58e61edbfafdbe553ea26120bcbcfe1e67f7df0
Pinger <pinger@xiaomingsport.com>
2019-02-02 11:30:28 +0800

    更新文档

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # 支付宝Android版支付SDK包装
+[ ![Download](https://api.bintray.com/packages/fungo/maven/alipay-sdk/images/download.svg) ](https://bintray.com/fungo/maven/alipay-sdk/_latestVersion)
 
-由于支付宝官方提供的支付SDK是AAR格式的，在Library中使用非常麻烦，
-需要每个子Module都添加AAR的路径，这里把AAR文件单独包装一层，生成依赖的方式，可以直接使用。
+由于支付宝官方提供的最新支付SDK是AAR格式的，在Library中使用需要各个module都配置aar路径，非常麻烦。
+为了避免这种到处配置的问题，这里把AAR文件添加到`jcenter`仓库，生成依赖的方式，可以直接引用。
 
 ### 使用
+在build.gradle文件中添加依赖
 
-### 依赖的支付宝SDK版本
-* alipaySdk-15.5.9-20181123210601
+        dependencies {
+            implementation 'com.pingerx:alipay-sdk:1.0.0'
+        }
+
+### 依赖的支付宝SDK
+* alipaySdk-15.5.9-20181123210601.aar
 
--- app/src/main/java/com/fungo/demo/MainActivity.kt
+++ app/src/main/java/com/fungo/demo/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.fungo.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alipay.sdk.app.PayTask
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        try {
+            PayTask(this)
+            tvStatus.text = "alipay-sdk调用成功"
+        } catch (e: Exception) {
+            tvStatus.text = "alipay-sdk调用失败"
+            e.printStackTrace()
+        }
     }
 }
